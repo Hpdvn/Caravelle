77fa990f1a5325ac71a63fd391993e2981e768fc
Hpdvn <hugop23080@gmail.com>
2021-11-11 00:06:40 +0100

    First Sound Recognition

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 try:
     import speech_recognition as sr
+    import pyaudio
 except ImportError:
-    print("Import failed.")
+    print(f"Import failed : {ImportError} ")
 finally:
     print("Done")
+
+r = sr.Recognizer()
+m = sr.Microphone()
+
+with m as source:
+    r.adjust_for_ambient_noise(source)
+    print("Say something!")
+    audio = r.listen(source)
+    print("Now recognizing...")
+    try:
+        print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+        print("Sphinx error; {0}".format(e))
+
+
+
+
